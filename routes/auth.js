af4bd6341778f381c94a232bf3f10987984eb404
ivan.easypyme <ivan.easypyme@gmail.com>
2026-03-09 10:55:14 +0100

    Refactor authentication routes to use single quotes for consistency; remove unused imports from auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const admin = require("../firebaseAdmin");
-const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 const authController = require("../controllers/authController");
 
-router.post("/google", authController.loginGoogle);
+router.post('/google', authController.loginGoogle);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
