8768ea4057fd9938beb697694cbcd1c12de50a27
ivan.easypyme <ivan.easypyme@gmail.com>
2026-03-23 19:47:52 +0100

    fix: update Google Maps API script loading to be asynchronous and enhance settings configuration

--- apps/battle-link/src/app/core/utils/google-maps-loader.ts
+++ apps/battle-link/src/app/core/utils/google-maps-loader.ts
@@ -2,7 +2,7 @@ export function loadGoogleMapsApi(apiKey: string): () => Promise<void> {
   return () =>
     new Promise<void>((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&loading=async`;
       script.async = true;
       script.defer = true;
       script.onload = () => resolve();
